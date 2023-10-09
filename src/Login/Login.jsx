6dc90255395e73c35fc99dd847c89544579e9268
saikat <tawhidulislam3482@gmail.com>
2023-10-09 21:48:47 +0600

    my project complete

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -34,7 +34,7 @@ const Login = () => {
   };
   return (
     <div>
-      <div className="hero   bg-base-200">
+      <div className="w-full md:hero  bg-base-200">
         <div className="hero-content flex-col mb-5">
           <div className="text-center">
             <h1 className="text-5xl font-bold">Login now!</h1>
